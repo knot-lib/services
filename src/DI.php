c9a0bcb0d79619694f3befe67900b541914d616f
Katsuki Shuto <stk2k@sazysoft.com>
2019-12-23 16:57:58 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- src/DI.php
+++ src/DI.php
@@ -12,31 +12,31 @@ class DI
     //==============================
     // Scheme
     //==============================
-    const SCHEME_COMPONENT        = 'component';
-    const SCHEME_SERVICE          = 'service';
-    const SCHEME_STRING           = 'string';
-    const SCHEME_ARRAY            = 'array';
+    const SCHEME_COMPONENT        = 'component://';
+    const SCHEME_SERVICE          = 'service://';
+    const SCHEME_STRING           = 'string://';
+    const SCHEME_ARRAY            = 'array://';
 
     //==============================
     // Object
     //==============================
-    const OBJECT_REQUEST          = 'request';
-    const OBJECT_RESPNSE          = 'response';
-    const OBJECT_FILESYSTEM       = 'filesystem';
-    const OBJECT_LOGGER           = 'logger';
-    const OBJECT_VALIDATION       = 'validation';
+    const REQUEST          = 'request';
+    const RESPNSE          = 'response';
+    const FILESYSTEM       = 'filesystem';
+    const LOGGER           = 'logger';
+    const VALIDATION       = 'validation';
 
     //==============================
     // Copmonents
     //==============================
-    const URI_COMPONENT_REQUEST        = self::SCHEME_COMPONENT . '://' . self::OBJECT_REQUEST;     //=> component://request
-    const URI_COMPONENT_RESPONSE       = self::SCHEME_COMPONENT . '://' . self::OBJECT_RESPNSE;     //=> component://response
+    const URI_COMPONENT_REQUEST        = self::SCHEME_COMPONENT . self::REQUEST;     //=> component://request
+    const URI_COMPONENT_RESPONSE       = self::SCHEME_COMPONENT . self::RESPNSE;     //=> component://response
 
     //==============================
     // Services
     //==============================
-    const URI_SERVICE_FILESYSTEM       = self::SCHEME_SERVICE . '://' . self::OBJECT_FILESYSTEM;     //=> service://filesystem
-    const URI_SERVICE_LOGGER           = self::SCHEME_SERVICE . '://' . self::OBJECT_LOGGER;         //=> service://logger
-    const URI_SERVICE_VALIDATION       = self::SCHEME_SERVICE . '://' . self::OBJECT_VALIDATION;     //=> service://validation
+    const URI_SERVICE_FILESYSTEM       = self::SCHEME_SERVICE . self::FILESYSTEM;     //=> service://filesystem
+    const URI_SERVICE_LOGGER           = self::SCHEME_SERVICE . self::LOGGER;         //=> service://logger
+    const URI_SERVICE_VALIDATION       = self::SCHEME_SERVICE . self::VALIDATION;     //=> service://validation
 
 }
